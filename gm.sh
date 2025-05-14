5dc9d286156e34cc46cbb45be8cb97f181dde069
luftEinz <luft0938@gmail.com>
2025-05-14 12:37:03 +0000

    Date: 2025-05-14 12:37:03\nhttps://mods.keays.pro/\n/workspaces/ArmaReforgerHowToMod

--- gm.sh
+++ gm.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-commit="\Date: $(date +%Y-%m-%d\ %H:%M:%S)"
-
-git add .
-git commit -m "$commit"
+commit="Date: $(date +%Y-%m-%d\ %H:%M:%S)"
+Site="https://mods.keays.pro/"
+project="$(pwd)"
+echo "Current directory: $project"
+echo "Current date and time: $(date +%Y-%m-%d\ %H:%M:%S)"
+echo "Current branch: $(git rev-parse --abbrev-ref HEAD)"
+echo "Current status: $(git status)"
+echo "$(ls -la)"
+git commit -m "$commit\n$Site\n$project"
 git push origin main
 git diff --cached
 echo "Changes have been committed and pushed to the main branch."
